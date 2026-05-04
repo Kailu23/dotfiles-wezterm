4782c1698e5a128d6183db9adb978f0ae8f65d31
Marin Valenčak <valencakmarin@gmail.com>
2026-05-04 13:45:32 +0200

    fix(startup): Open WezTerm here
        - Wasn't opening in CWD

--- wezterm.lua
+++ wezterm.lua
@@ -20,7 +20,7 @@ config.allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace"
 
 config.use_cap_height_to_scale_fallback_fonts = true
 
-wezterm.on("gui-startup", function (window)
+wezterm.on("gui-startup", function (cmd)
     local tab, pane, window = mux.spawn_window(cmd or {})
     local gui_window = window:gui_window();
     gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
