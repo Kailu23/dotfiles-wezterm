6ba0c4114b00012c1e8c2bfd1d719c3677f89b86
Marin Valenčak <valencakmarin@gmail.com>
2026-05-05 14:48:17 +0200

    chore(wezterm): change copy mode keybind

--- wezterm.lua
+++ wezterm.lua
@@ -6,8 +6,8 @@ local config = wezterm.config_builder()
 config.default_prog = { "bash.exe", "--login", "-i" }
 
 config.font = wezterm.font_with_fallback({
-	"Hack Nerd Font Mono",
-	"Segoe UI Emoji",
+    "Hack Nerd Font Mono",
+    "Segoe UI Emoji",
 })
 
 config.color_scheme = "Monokai Soda (Gogh)"
@@ -20,10 +20,17 @@ config.allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace"
 
 config.use_cap_height_to_scale_fallback_fonts = true
 
-wezterm.on("gui-startup", function (cmd)
+wezterm.on("gui-startup", function(cmd)
     local tab, pane, window = mux.spawn_window(cmd or {})
-    local gui_window = window:gui_window();
+    local gui_window = window:gui_window()
     gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
 end)
 
+config.keys = {
+    {
+        key = "x",
+        mods = "CTRL",
+        action = wezterm.action.ActivateCopyMode,
+    },
+}
 return config
