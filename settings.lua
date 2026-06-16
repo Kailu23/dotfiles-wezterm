7b11e9795860e80ca5237221bc8bea527982a3bf
Marin Valenčak <valencakmarin@gmail.com>
2026-06-17 01:26:12 +0200

    style(ui): improve ui
        - darken unfocused panes
        - change default workspace name from "default" to "home"
        - disable fancy tab bar
        - now applies theme onto the tab bar
        - add status so the user has visual feedback on what is active

--- settings.lua
+++ settings.lua
@@ -9,16 +9,54 @@ function M.apply(config)
 
     config.font_size = 14.0
 
+    -- config.font = wezterm.font_with_fallback({
+    --     "Hack Nerd Font Mono",
+    --     "Segoe UI Emoji",
+    -- })
+
     config.allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace"
 
     config.use_cap_height_to_scale_fallback_fonts = true
 
-    config.window_close_confirmation = "NeverPrompt"
+    config.window_close_confirmation = "AlwaysPrompt"
+    config.window_background_opacity = 0.9
+    config.window_decorations = "RESIZE"
 
-    config.font = wezterm.font_with_fallback({
-        "Hack Nerd Font Mono",
-        "Segoe UI Emoji",
-    })
+    config.inactive_pane_hsb = {
+        saturation = 0.75,
+        brightness = 0.5,
+    }
+
+    config.default_workspace = "home"
+
+    config.use_fancy_tab_bar = false
+    config.tab_bar_at_bottom = true
+
+    config.status_update_interval = 1000
+    wezterm.on("update-status", function(window, pane)
+        local workspace = window:active_workspace()
+        local status = ""
+
+        if window:active_key_table() then
+            status = window:active_key_table()
+        end
+        if window:leader_is_active() then
+            status = "PREFIX "
+        end
+
+        local basename = function(fullName)
+            -- return string.gsub(fullName, "(.*[/\\])(.*)", "%2")
+        end
+
+        -- local cwd = pane:get_current_working_dir()
+
+        window:set_left_status(wezterm.format({
+            { Text = " " .. wezterm.nerdfonts.oct_table .. " " .. workspace .. " " },
+        }))
+        window:set_right_status(wezterm.format({
+            { Text = status},
+        }))
+    end)
 end
 
 return M
