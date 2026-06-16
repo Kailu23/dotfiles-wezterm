3d20054b088ef755a7dbc768011c1beb7e8c0e19
Marin Valenčak <valencakmarin@gmail.com>
2026-06-17 01:31:16 +0200

    feat(keys): add tmux-like keymaps

--- keys.lua
+++ keys.lua
@@ -2,16 +2,64 @@ local M = {}
 
 function M.apply(config)
     local wezterm = require("wezterm")
+    local act = wezterm.action
+
+    config.leader = {
+        key = "Space",
+        mods = "CTRL",
+        timeout_milliseconds = 2000,
+    }
     config.keys = {
+        { key = "Space", mods = "LEADER", action = act.SendKey({ key = "Space", mods = "CTRL" }) },
+        { key = "[", mods = "LEADER", action = act.ActivateCopyMode },
+        { key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+        { key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+        { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+        { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+        { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+        { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+        { key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = true }) },
+        { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+        { key = "s", mods = "LEADER", action = act.RotatePanes("Clockwise") },
         {
-            key = "x",
-            mods = "CTRL",
-            action = wezterm.action.ActivateCopyMode,
+            key = "r",
+            mods = "LEADER",
+            action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }),
         },
+        { key = "c", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+        { key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+        { key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+        { key = "t", mods = "LEADER", action = act.ShowTabNavigator },
         {
-            key = "W",
-            mods = "CTRL",
-            action = wezterm.action.CloseCurrentTab({ confirm = false }),
+            key = "m",
+            mods = "LEADER",
+            action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }),
+        },
+        { key = "w", mods = "LEADER", action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES"} },
+    }
+
+    for i = 1, 9 do
+        table.insert(config.keys, {
+            key = tostring(i),
+            mods = "LEADER",
+            action = act.ActivateTab(i - 1)
+        })
+    end
+
+    config.key_tables = {
+        resize_pane = {
+            { key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+            { key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+            { key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+            { key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+            { key = "Escape", action = "PopKeyTable" },
+            { key = "Enter", action = "PopKeyTable" },
+        },
+        move_tab = {
+            { key = "h", action = act.MoveTabRelative(-1) },
+            { key = "l", action = act.MoveTabRelative(1) },
+            { key = "Escape", action = "PopKeyTable" },
+            { key = "Enter", action = "PopKeyTable" },
         },
     }
 end
