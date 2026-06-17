5fa2999b9da30fa8a8d9e9228f473a350c96c417
Marin Valenčak <valencakmarin@gmail.com>
2026-06-17 02:52:41 +0200

    feat(wezterm): add new keybinds
        - add keybinds for renaming the current tab, creating new workspace
          and creating default tab with custom names

--- keys.lua
+++ keys.lua
@@ -35,14 +35,68 @@ function M.apply(config)
             mods = "LEADER",
             action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }),
         },
-        { key = "w", mods = "LEADER", action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES"} },
+        { key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
+        {
+            key = ",",
+            mods = "LEADER",
+            action = act.PromptInputLine({
+                description = "Enter new name for tab",
+                initial_value = "",
+                action = wezterm.action_callback(function(window, pane, line)
+                    -- line will be `nil` if they hit escape without entering anything
+                    -- An empty string if they just hit enter
+                    -- Or the actual line of text they wrote
+                    if line then
+                        window:active_tab():set_title(line)
+                    end
+                end),
+            }),
+        },
+        {
+            key = "$",
+            mods = "LEADER|SHIFT",
+            action = act.PromptInputLine({
+                description = wezterm.format({
+                    { Attribute = { Intensity = "Bold" } },
+                    { Foreground = { AnsiColor = "Fuchsia" } },
+                    { Text = "Enter name for new workspace" },
+                }),
+                action = wezterm.action_callback(function(window, pane, line)
+                    -- line will be `nil` if they hit escape without entering anything
+                    -- An empty string if they just hit enter
+                    -- Or the actual line of text they wrote
+                    if line then
+                        window:perform_action(
+                            act.SwitchToWorkspace({
+                                name = line,
+                            }),
+                            pane
+                        )
+                    end
+                end),
+            }),
+        },
+        {
+            key = "W",
+            mods = "LEADER|SHIFT",
+            action = wezterm.action_callback(function(window, pane)
+                local tab = window:mux_window():active_tab()
+                tab:set_title("nvim")
+
+                local ssh_tab = window:mux_window():spawn_tab({})
+                ssh_tab:set_title("ssh")
+
+                local git_tab = window:mux_window():spawn_tab({})
+                git_tab:set_title("git")
+            end),
+        },
     }
 
     for i = 1, 9 do
         table.insert(config.keys, {
             key = tostring(i),
             mods = "LEADER",
-            action = act.ActivateTab(i - 1)
+            action = act.ActivateTab(i - 1),
         })
     end
 
