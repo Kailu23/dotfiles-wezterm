03874e995e4a98c8d9e1bc1444da2fb388444f5c
Marin Valenčak <valencakmarin@gmail.com>
2026-07-31 22:10:42 +0200

    fix(shell): switch bash to zsh
        - wezterm still had the bash shell for linux config

--- platform/linux.lua
+++ platform/linux.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 function M.apply(config)
-    config.default_prog = { "bash", "--login", "-i" }
+    config.default_prog = { "zsh", "--login", "-i" }
     config.enable_wayland = true
     config.window_decorations = "NONE"
     config.color_scheme = "Catppuccin Macchiato"
